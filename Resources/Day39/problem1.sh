b1e4b8a1bf015b4804993c0aff0e49972ea74414
piyush sachdeva <40286378+piyushsachdeva@users.noreply.github.com>
2024-08-26 09:04:10 -0400

    Update problem1.sh

--- Resources/Day39/problem1.sh
+++ Resources/Day39/problem1.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-# Wait for control-plane node to transition into the "Ready" status
-while true; do 
-  JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}' && kubectl get nodes -o jsonpath="$JSONPATH" | grep "Ready=True" | grep "Ready=True" &> /dev/null
-  if [[ "$?" -ne 0 ]]; then
-    sleep 5
-  else
-    break
-  fi
-done
 
 # Misconfigure kubelet configuration
 sudo sed -i 's/clientCAFile: \/etc\/kubernetes\/pki\/ca.crt/clientCAFile: \/etc\/kubernetes\/pki\/non-existent-ca.crt/g' /var/lib/kubelet/config.yaml
