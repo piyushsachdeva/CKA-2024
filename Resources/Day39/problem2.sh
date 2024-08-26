63d584364e9fe07ebe184f72cb15884691db0aba
piyush sachdeva <40286378+piyushsachdeva@users.noreply.github.com>
2024-08-26 09:04:24 -0400

    Update problem2.sh

--- Resources/Day39/problem2.sh
+++ Resources/Day39/problem2.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-# Wait for control-plane node to transition into the "Ready" status
-while true; do 
-  JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}' && kubectl get nodes -o jsonpath="$JSONPATH" | grep "Ready=True" | grep "Ready=True" &> /dev/null
-  if [[ "$?" -ne 0 ]]; then
-    sleep 5
-  else
-    break
-  fi
-done
-
 
 # Stop kubelet 
 sudo systemctl stop kubelet
